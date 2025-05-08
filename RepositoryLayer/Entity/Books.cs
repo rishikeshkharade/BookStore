640fd8aed610a608dc5ca4d3dc310d1c028fa0cb
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-05-09 00:39:58 +0530

    [Rishikesh] Books:- Added CRUD operations base on Role

---
+++ RepositoryLayer/Entity/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RepositoryLayer.Entity
+{
+    public partial class Books
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int DiscountPrice { get; set; }
+        public int Quantity { get; set; }
+        public string BookImage { get; set; }
+        public int AdminId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ RepositoryLayer/Services/BookRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+
+namespace RepositoryLayer.Services
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreDbContext _context;
+        public BookRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public Books AddBook(Books book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return book;
+        }
+        public Books UpdateBook(int bookId, Books updatedbook)
+        {
+            var book = _context.Books.FirstOrDefault(b => b.BookId == bookId);
+            if (book == null) return null;
+            
+            book.BookName = updatedbook.BookName;
+            book.Author = updatedbook.Author;
+            book.Price = updatedbook.Price;
+            book.Quantity = updatedbook.Quantity;
+            book.Description = updatedbook.Description;
+            book.BookImage = updatedbook.BookImage;
+            book.DiscountPrice = updatedbook.DiscountPrice;
+            book.UpdatedAt = DateTime.Now;
+  
+            _context.SaveChanges();
+            return book;
+        }
+        public bool DeleteBook(int bookId)
+        {
+            var book = _context.Books.Find(bookId);
+            if (book == null) return false;
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return true;
+        }
+        public Books GetBookById(int bookId)
+        {
+            return _context.Books.Find(bookId);
+        }
+        public IEnumerable<Books> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+    }
+}
